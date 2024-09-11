4b960657a043a9a01081367ebf3d1492121f117e
ricardoacruche <richardbusiness29@aol.com>
2024-09-11 10:40:27 -0300

    Add files via upload

---
+++ aula2/converter.py
@@ -0,0 +1,16 @@
+n1 = 4 #int
+n2 = 3.5 #tipo decimal (float)
+a = float (n1)
+print (a)
+print (type (a))
+b = int (n2)
+print (b)
+print (type (b))
+
+meu_texto = "senac botafogo"
+print (meu_texto)
+
+#Podemos juntar, o que chamos de concatenar
+print (f'estou estudando no {meu_texto} agora')
+print ( meu_texto.capitalize ())
+print (meu_texto.upper ())
---
+++ aula2/funcaobiblioteca.py
@@ -0,0 +1,5 @@
+import math 
+
+print (math.floor (8.99999))
+print (math.ceil (8.99999))
+print (math.sqrt (8.144)) #raiz quadrada
